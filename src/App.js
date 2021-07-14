1e32394f5e1840448af6414f4a012c5a438e0f1e
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-14 10:51:26 -0600

    added dependencies: , react-bootsrap, bootsratp, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 
+
 function App() {
+
+
   return (
     <>
-      <nav>
-        Home | News | About | Contact
-      </nav> 
-      <div className="container">
-        Hi!
-      </div>
+
     </>
   );
 }
---
+++ src/PageHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PageHome() {
+    return (
+        <div className="container">
+            <p>Hi!</p>
+            <p>This is the home page</p>
+        </div>
+    )
+}
